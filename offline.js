b578040a8bea7fb2bad8719f9d3a4e4c65adbe61
KrahoKanela <66818070+KrahoKanela@users.noreply.github.com>
2020-11-18 23:07:00 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1605750708,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"tiledbackground2.png",
+		"tiledbackground.png",
+		"spritebox-sheet0.png",
+		"spritebuttonstart_old-sheet0.png",
+		"spritebuttonstart-sheet0.png",
+		"mat100-sheet0.png",
+		"mat10-sheet0.png",
+		"mat1-sheet0.png",
+		"marshmello, bastille happier lyrics-[convertirvideo.com].ogg",
+		"panic! at the disco  - into the unknown (lyrics).ogg",
+		"paradise - coldplay [lyrics].ogg",
+		"alessia cara - i choose (lyrics).ogg",
+		"happy - pharrell williams (lyrics) 🎵.ogg",
+		"loading-logo.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"app-logo.png",
+		"logo.png"
+	]
+}
